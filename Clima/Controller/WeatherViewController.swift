c18f3eec179cec39e91a8f42bceeabf15ac5eb90
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-09-09 15:04:39 -0400

    Handling of the TextField.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,53 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // TextField has to tell to the ViewController what is happening
+        searchTextField.delegate = self
     }
 
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        // Dismisses the keyboard
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    // Handle when the user presses the return button
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Dismisses the keyboard
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    // When the search button or the return button is pressed
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    // Delete the text in the textField when the user finished editing
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Use searchTextField.text to get the weather for that city
+        
+        // Emptying the textField
+        searchTextField.text = ""
+        // Reset the placeholder of the textField
+        textField.placeholder = "Search"
+    }
 }
 
